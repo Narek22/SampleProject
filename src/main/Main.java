e7566b9e965afa5a0d2303eae83992500e904329
Narek <narek_musakhanyan@epam.com>
2017-10-18 14:33:45 +0400

    commits

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,13 @@ package main;
 
 public class Main {
     public static void main(String args[]) {
-//        long x = 1125336121L;
-//        int y = 1563;
-//        y *= x;
-//        System.out.println(y);
+        long x = 1125336121L;
+        int y = 1563;
+        y *= x;
+        System.out.println(y);
 
 
-        A a = new A();
-        System.out.println(a instanceof B);
+//        A a = new A();
+//        System.out.println(a instanceof B);
     }
 }
